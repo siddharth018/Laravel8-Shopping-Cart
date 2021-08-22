78e2bcff12867bfcf77f9fe88fa1260f68f9de86
siddharth018 <siddharthshukla089@gmail.com>
2021-08-23 01:22:51 +0530

    update code

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', [ProductController::class, 'productList'])->name('products.list');
 Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
 Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
 Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
 Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
 Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
+
